36d7688c5cd111c60c93ce7e5fd13f9ab08be422
Sergey Koshelev <sergey.k@nopcommerce.com>
2026-08-11 11:31:24 +0300

    Issue 8270. Remove obsolete ProductAttributeCombination.PictureId and ProductAttributeValue.PictureId properties

--- src/Libraries/Nop.Data/Migrations/UpgradeTo500/SchemaMigration.cs
+++ src/Libraries/Nop.Data/Migrations/UpgradeTo500/SchemaMigration.cs
@@ -1,7 +1,7 @@
 ﻿using FluentMigrator;
 using Nop.Core.Domain.Catalog;
-using Nop.Core.Domain.Customers;
 using Nop.Core.Domain.Common;
+using Nop.Core.Domain.Customers;
 using Nop.Core.Domain.Orders;
 using Nop.Data.Extensions;
 
@@ -102,5 +102,9 @@ public class SchemaMigration : ForwardOnlyMigration
 
         //#4279
         this.CreateTableIfNotExists<Product3dObject>();
+
+        //#8270
+        this.DeleteColumnsIfExists<ProductAttributeCombination>(["PictureId"]);
+        this.DeleteColumnsIfExists<ProductAttributeValue>(["PictureId"]);
     }
 }
